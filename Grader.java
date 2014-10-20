e33d852ab759f2f50206ab4f90e06090303fa1a7
laviy <yonatan_lavi@asl.org>
2014-10-20 12:15:32 +0100

    Completed midterm

--- Grader.java
+++ Grader.java
@@ -1,32 +1,28 @@
 /**
-*  Calculates the grade a student receives on an examination
-*
-*  The program first prompts the user to enter the number of points possible
-*  on the exam.  Then, the program prompts for the number of items missed; if a positive
-*  number is entered, the program responds by calculating and displaying the percentage
-*  score out of 100.  If a negative number is entered, the program prints a "Thank You" message
-*  and exits.
-*  
-*  A sample interaction might be: (user input marked with '>')
-*  "Enter the number of items on the exam"
-*  >25
-*  "Enter the number of items missed"
-*  >3.5
-*  "Missing 3.5 results in a score of 86.0"
-*  "Enter the number of items missed"
-*  >0
-*  "Missing 0 results in a score of 100.0"
-*  "Enter the number of items missed"
-*  >-100
-*  "Thank you -- have a great day!"
-*  
-*/
+*  Yoni Lavi
+*  20 October, 2014
+*/ 
+
+import java.util.Scanner;
 
 public class Grader {
   
   public static void main(String[] args) {
-    //TODO insert your code here
+     Scanner scnr = new Scanner(System.in);
+     double examItems = 0;
+     double missedItems = 0;
+     double percent = 0;
+     
+     System.out.println("Please enter the number of points on the exam.");
+     examItems = scnr.nextInt();
+     while (missedItems >=0) {
+         System.out.println("Please enter the number of points missed on the exam.");
+         missedItems = scnr.nextInt();
+         percent = ((examItems - missedItems)/examItems) * 100;
+         System.out.println("Missing " + missedItems + " points on the exam results in a score of " + percent);
+         }
+     System.out.println("Thank you, have a great day!"); 
+      
   }
-  
-  
+    
 }
--- PizzaPalace.java
+++ PizzaPalace.java
@@ -1,26 +1,31 @@
 /**
- * Determines the number of pizza pies required to feed an after school club.
- * 
- * Basic program
- * 	Prompt the user to enter the number of people to be fed, the number of
- * 	slices allowed for each person, and the number of slices in each whole pie.
- * 	Given these inputs, determine and display the number of pizzas to purchase.
- * 
- * For guidance, refer to the flowchart PizzaPalace.pdf distributed with this file
- * 
- * @author <change to your name>
- * @version <today's date>
+ * Yoni Lavi 
+ * 20 October, 2014 
  */
+      
+import java.util.Scanner; 
+ 
 public class PizzaPalace {
 
 	public static void main(String[] args) {
-		// TODO Replace this section with your own code
+      Scanner scnr = new Scanner(System.in);
+      double people = 0;
+      double slicesAllowed = 0;
+      double numslices = 0;
+      double numpizzas = 0;
+      
+      System.out.println("Please enter the number of people at the club.");
+      people = scnr.nextInt();
+      System.out.println("Please enter the number of slices each person is allowed to eat.");
+      slicesAllowed = scnr.nextInt();
+      System.out.println("Please enter the number of slices in each whole pie.");
+      numslices = scnr.nextInt();
+      
+      numpizzas = (people * slicesAllowed)/numslices;
+      numpizzas = Math.ceil(numpizzas);
+      
+      System.out.println("You would need to buy " + numpizzas + " pizzas to feed everyone at the party.");
 		
-		// NOTE
-		// The Math class provides a function, ceiling(), which will take a double
-		// and return the next higher integer value.
-		// For example, Math.ceiling(4.125) would evaluate to 5
-
 	}
 
 }
